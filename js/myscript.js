c69f7d36f9bc20bc709ed45db52c1d7020880099
kelluhubert <irakellyhub@gmail.com>
2023-05-05 21:51:08 +0200

    a javascript turtarial

---
+++ js/myscript.js
@@ -0,0 +1,17 @@
+// function lifeInYear(age){
+//     var age = prompt("enter your ag")
+//     var yearsRemaning = 90 - age;
+//     var dayRemaning = yearsRemaning * 365;
+//     var weekRemaning = yearsRemaning * 52;
+//     var monthRemaining = yearsRemaning * 12;
+//     alert("you have"+yearsRemaning+"and"+monthRemaining+"month remaining, "+ weekRemaning +" week remaining, "+ dayRemaning +"days remaining.");
+// }
+// lifeInYear(25);
+ 
+function getbottel(money){
+var bottel= prompt("enter the bootel you want");
+var priceperBottel= prompt("enter the price per bottel"):
+var calcmoney= bottel*priceperBottel;
+alert(calcmone) ;
+}
+getbottel();
